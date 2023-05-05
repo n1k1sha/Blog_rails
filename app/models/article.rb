9cd1b2ec58bac9c622171862b6fb5633e285c6ea
Nikita Shamov <vomash_0@gmail.com>
2023-05-05 21:22:24 +0300

    Setting models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
+
+    has_many :comments, dependent: :destroy
+
+    belongs_to :user
+
     has_one_attached :photo
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+    
 end
