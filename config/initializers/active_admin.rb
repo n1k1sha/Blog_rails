081c228f04e18716716bd521c2d50fb3bb90a10e
Nikita Shamov <vomash_0@gmail.com>
2023-05-05 21:06:23 +0300

    Installing and setting ArcticAdmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,11 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "App"
+  config.site_title = "Blog"
+
+  meta_tags_options = { viewport: 'width=device-width, initial-scale=1' }
+  config.meta_tags = meta_tags_options
+  config.meta_tags_for_logged_out_pages = meta_tags_options
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
