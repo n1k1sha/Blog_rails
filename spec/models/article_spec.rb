f4f919e0f0990463fcc95d2eb0f8ad313e3ff162
Nikita <vomash0@gmail.com>
2023-05-14 16:16:49 +0300

    Add test from article_model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article1) { build(:article) }
+  
+  it "is valid with valid attributes" do
+    expect(article1).to be_valid
+  end
+  
+  it "is empty for title" do
+    article2 = build(:article, title: "")
+    expect(article2).to_not be_valid
+  end
+  
+  it "is empty for body" do
+    article2 = build(:article, body: "")
+    expect(article2).to_not be_valid
+  end
+  
+  it "is nil for title" do 
+    article2 = build(:article, title: nil)
+    expect(article2).to_not be_valid
+  end
+  
+  it "is nil for body" do 
+    article2 = build(:article, body: nil)
+    expect(article2).to_not be_valid
+  end
 end
